887cf50edbc8218a6b38aab928b69abf8f324ce1
zaibmohd <32215085+zaibmohd@users.noreply.github.com>
2017-09-23 20:01:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,10 +17,10 @@ app.get('/article-two', function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 }); 
 app.get('/article-three', function (req,res) {
-    res.send('Article three will be requested and will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 }); 
 app.get('/article-one', function (req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send('Article one will be requested and will be served here');
 }); 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
