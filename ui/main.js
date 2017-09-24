e7889dc467ff65ab19cb06880c768c53f26631f3
zaibmohd <32215085+zaibmohd@users.noreply.github.com>
2017-09-24 19:48:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,19 @@
 // counter code
 var  button= document.getElementById('counter');
-var counter = 0;
     
     button.onclick= function () {
         
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString(); 
-};
+        var request= newXMLHttpRequest();
+        request.onreadystatechange = function (){
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status===200) {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                     span.innerHTML = counter.toString(); 
+                }
+            }
+        };
+        
+        request.open('GET', 'http://zaibmohd07.imad.hasura-app.io/counter',true);
+        request.send(null);
+    };
